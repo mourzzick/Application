26d5b229aa84ec742ada0232c671f19f4700c52e
Ruslan <ruslan.georgiev@hotmail.com>
2018-03-23 16:09:50 +0100

    Initial commit

---
+++ src/Hash.java
@@ -0,0 +1,59 @@
+import javax.xml.bind.DatatypeConverter;
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.ArrayList;
+
+public class Hash {
+    public static void main(String[] args) throws IOException, NoSuchAlgorithmException {
+
+        long startTime = System.nanoTime();
+
+
+
+
+
+
+        File file = new File("C:\\Users\\Ruslan\\Desktop\\New folder");
+        File[] files = file.listFiles();
+        ArrayList<String> hashes = new ArrayList<>();
+        ArrayList<String> notDupHashes = new ArrayList<>();
+        for (File element : files) {
+            Path path = Paths.get(element.getAbsolutePath());
+
+            if (path.toString().contains(".jpg")) {
+                MessageDigest md = MessageDigest.getInstance("MD5");
+                md.update(Files.readAllBytes(path));
+                byte[] digest = md.digest();
+                String myMD5 = DatatypeConverter.printHexBinary(digest).toUpperCase();
+                System.out.println(myMD5);
+                hashes.add(myMD5);
+            }
+
+
+        }
+
+        for (String str : hashes) {
+            if (notDupHashes.contains(str)) {
+                System.out.println("Yes");
+            } else {
+                notDupHashes.add(str);
+                System.out.println(str);
+            }
+        }
+
+        long endTime = System.nanoTime();
+
+        long duration = (endTime - startTime);
+        System.out.println(duration);
+
+
+
+    }
+
+
+}
---
+++ src/Test.java
@@ -0,0 +1,47 @@
+import java.io.File;
+import java.util.ArrayList;
+
+public class Test {
+    public static int fold = 0;
+    public static int fil = 0;
+    public static ArrayList<String> size = new ArrayList<>();
+
+    //File[] listOfFiles = folder.listFiles();
+
+    public static void main(String[] args) {
+        File folder = new File("D:\\Ruslan\\Photos Go Here");
+        listDir(folder);
+        System.out.println("There was " + Test.fold + " folders");
+        System.out.println("There was " + Test.fil + " files");
+        for (int i = 0; i < Test.size.size(); i++) {
+            System.out.println(Test.size.get(i));
+        }
+
+
+
+    }
+
+    public static void listDir(File folder) {
+
+        File[] listOfFiles = folder.listFiles();
+        for (int i = 0; i < listOfFiles.length; i++) {
+            if (listOfFiles[i].isFile()) {
+                Test.size.add(listOfFiles[i].getName());
+                System.out.println(listOfFiles[i].getAbsolutePath());
+                Test.fil++;
+            } else {
+                listDir(listOfFiles[i]);
+                System.out.println("FOLDER");
+                Test.fold++;
+            }
+
+
+        }
+
+
+
+
+    }
+
+
+}
